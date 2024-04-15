7cb4604cf230e5cf4485fe744544ec31d10269f3
Tashmeet Kaur Hora <95216790+Tashmeet29@users.noreply.github.com>
2024-04-15 23:57:59 +0530

    Add files via upload

---
+++ Assignment 1/Food project/With Bootstrap/If_Else.js
@@ -0,0 +1,23 @@
+const temperature = 25;
+
+if(temperature > 30) {
+    console.log("It's a hot day!");
+} else {
+    console.log("It's a not hot day!");
+}
+
+//Output:
+//It's a not hot day!
+
+const hour = 14;
+
+if(hour<12) {
+    console.log("good morning!");
+} else if(hour<10) {
+    console.log("good afternoon!");
+} else {
+    console.log("good evening!");
+}
+
+//Output:
+//good evening!
